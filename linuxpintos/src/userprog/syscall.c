f6cb10a3517190a800b5a7e494149a7d50f0b3f8
Alexander Zeijlon <aleze075@student.liu.se>
2018-01-25 17:51:26 +0100

    started on create() syscall.

--- linuxpintos/src/userprog/syscall.c
+++ linuxpintos/src/userprog/syscall.c
@@ -5,16 +5,36 @@
 #include "threads/thread.h"
 
 static void syscall_handler (struct intr_frame *);
+bool create (void *esp);
+
 
 void
-syscall_init (void) 
+syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f UNUSED)
 {
-  printf ("system call!\n");
-  thread_exit ();
+  // Get syscall nr.
+  uint32_t syscall_no = (uint32_t) f->esp;
+
+  switch (syscall_no) {
+    case SYS_HALT:
+    power_off();
+    break;
+
+    case SYS_CREATE:
+    f->eax = create(f->esp);
+    break;
+  }
+  //printf ("system call!\n");
+  //thread_exit ();
+}
+
+bool create(void *esp) {
+  const char *file = (const char *) (esp + 4);
+  uint32_t initial_size = *((uint32_t *) (esp + 8));
+  return true;
 }
--- linuxpintos/src/userprog/syscall.h
+++ linuxpintos/src/userprog/syscall.h
@@ -3,4 +3,13 @@
 
 void syscall_init (void);
 
+/* Our syscall definitions */
+
+
+/*int open (const char *file);
+void close (int fd);
+int read (int fd, void *buffer, unsigned size);
+int write (int fd, const void *buffer, unsigned size);
+void exit (int status);
+*/
 #endif /* userprog/syscall.h */
